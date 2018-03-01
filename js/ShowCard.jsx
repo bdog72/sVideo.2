1b38c3ff007e4245983932987afba7f5fd96cc34
Brian Bycynski <high5bri@hotmail.com>
2018-03-01 06:53:32 -0500

    going to refactor with redux

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,16 +1,19 @@
 // @flow
 
-import React from 'react';
-import { string } from 'prop-types';
+import React, { Component } from 'react';
+// import { string } from 'prop-types';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom'
 
-const Wrapper = styled.div`
+const Wrapper = styled(Link)`
     width: 32%;
     border: 2px solid #333;
     border-radius: 4px;
     margin-bottom: 25px;
     padding-right: 10px;
     overflow: hidden;
+    color: black;
+    text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -20,25 +23,36 @@ const Image = styled.img`
 
 `;
 
-const ShowCard = props => (
-    <Wrapper>
-        <Image
-            alt={`${props.title} Show Poster`}
-            src={`/public/img/posters/${props.poster}`}
-        />
-        <div>
-            <h3>{props.title}</h3>
-            <h4>({props.year})</h4>
-            <p>{props.description}</p>
-        </div>
-    </Wrapper>
-);
+class ShowCard extends Component {
+  shouldComponentUpdate () {
+    return false
+  }
+  
+  props: Show
+  render() {
+    return (
+      <Wrapper to={`/details/${this.props.imdbID}`}>
 
-ShowCard.propTypes = {
-    poster: string.isRequired,
-    title: string.isRequired,
-    year: string.isRequired,
-    description: string.isRequired
-};
+            <Image
+                alt={`${this.props.title} Show Poster`}
+                src={`/public/img/posters/${this.props.poster}`}
+            />
+            <div>
+                <h3>{this.props.title}</h3>
+                <h4>({this.props.year})</h4>
+                <p>{this.props.description}</p>
+            </div>
+      </Wrapper>
+    )
+  }
+}
+
+
+// ShowCard.propTypes = {
+//     poster: string.isRequired,
+//     title: string.isRequired,
+//     year: string.isRequired,
+//     description: string.isRequired
+// };
 
 export default ShowCard;
